a122be447fb6e68dd49726a24aeb6cf00dfa7d55
Syartaa <syartapajaziti@gmail.com>
2024-12-04 16:16:38 +0100

    updates

--- client/src/Layouts/user/pages/sigup.js
+++ client/src/Layouts/user/pages/sigup.js
@@ -64,13 +64,7 @@ export default function Signup() {
     <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
       <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          <div>
-            <img
-              src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-              className="w-32 mx-auto"
-              alt="Logo"
-            />
-          </div>
+          
           <div className="mt-12 flex flex-col items-center">
             <h1 className="text-2xl xl:text-3xl font-extrabold">Sign Up</h1>
             {error && <p className="text-red-500">{error}</p>}
@@ -125,12 +119,12 @@ export default function Signup() {
                     )}
                   </button>
                   <p className="mt-6 text-xs text-gray-600 text-center">
-                    Have an account
+                    Have an account 
                     <a
                       href="/login"
                       className="border-b border-gray-500 border-dotted"
                     >
-                      Login
+                       Login
                     </a>
                   </p>
                 </div>
